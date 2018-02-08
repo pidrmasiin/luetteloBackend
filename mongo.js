cc928ad85612355caebd1525ad24a0f918ea6782
pidrmasiin <petteri.paakkonen@helsinki.fi>
2018-02-08 12:26:52 +0200

    initiate app.

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+// korvaa url oman tietokantasi urlilla. ethän laita salasanaa Gothubiin!
+const url = 'mongodb://luettelo:xxx@ds125388.mlab.com:25388/luettelo'
+
+mongoose.connect(url)
+
+var name = process.argv[2];
+var number = process.argv[3];
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+const person = new Person({
+  name: name,
+  number: number
+})
+
+
+if(name === undefined){
+    Person
+        .find({})
+        .then(result => {
+            console.log('puhelinluettelo:')
+            result.forEach(person => {
+            console.log(person.name + ' ' + person.number)
+        })
+        mongoose.connection.close()
+  })
+}else{
+person
+  .save()
+  .then(response => {
+        console.log('lisätään henkilö '+ 
+        name + ' numero ' + number
+        + ' luetteloon')
+            
+        mongoose.connection.close()
+  })
+}
